3862566f3ea91fb78e914d7c4d4c1582b0a2e757
Franek Pikon <pikonfranek@student.agh.edu.pl>
2025-03-18 14:40:57 +0100

    lab_3_obowiazkowe_done_dodatkowe_dokonczyc

--- LAB_03/rozwinieta_lista_wiazana.py
+++ LAB_03/rozwinieta_lista_wiazana.py
@@ -6,7 +6,8 @@ class Node:
         self.next = None
 
     def __str__(self):
-        return str(self.tab[:self.count])
+        return str(self.tab)
+        # return str(self.tab[:self.count])
 
 class ULL:
     def __init__(self, size):
@@ -15,40 +16,91 @@ class ULL:
 
     def get(self, index):
         current = self.__head
-        while index > self.__size:
-            index -= current.count
+        while index > current.count:
             if current.next is None:
                 raise Exception("Index out of range")
+
+            index -= current.count
             current = current.next
         return current.tab[index]
 
     def insert(self, index, value):
         current = self.__head
-        while index > self.__size:
-            index -= current.count
+        # przejście do odpowiedniej listy
+        while index > current.count:
             if current.next is None:
-                raise Exception("Index out of range")
+
+                break
+            index -= current.count
             current = current.next
 
-        return value
+        # powiększenie listy jak jest pełna
+        if current.count == self.__size:
+            half_size = self.__size // 2
+            new_node = Node(self.__size)
+            new_node.next = current.next
+            current.next = new_node
+            new_node.tab[0:half_size] = current.tab[half_size:]
+            current.tab[half_size:] = [None for _ in range(half_size)]
+            current.count = half_size
+            new_node.count = half_size
+
+            # wybór odpowiedniej listy do dopisania
+            if index >= half_size:
+                index -= half_size
+                current = current.next
+
+        # dodawanie elementu do listy
+        if index > current.count:
+            current.tab[current.count] = value
+        else:
+            current.tab[index+1:] = current.tab[index:-1]
+            current.tab[index] = value
+
+        current.count += 1
+
+    def delete(self, index):
+        current = self.__head
+        while index > current.count:
+            if current.next is None:
+                raise Exception("Index out of range")
+            index -= current.count
+            current = current.next
+
+        temp = current.tab[index+1:]
+        temp.append(None)
+        current.tab[index:] = temp
+
+        current.count -= 1
+
+
 
     def __str__(self):
         if self.__head is None:
-            return 'ListaWiazana is empty'
+            return 'List is empty'
         else:
             str_list = [""]
             current = self.__head
             str_list.append(str(current))
             while current.next is not None:
                 current = current.next
-                str_list.append(str(current.data))
+                str_list.append(str(current.tab))
             return "\n-> ".join(str_list)
 
 
 def main():
-    l = ULL(5)
+    l = ULL(6)
+    for i in range(9):
+        l.insert(i+1, i+1)
+        print(l)
+    print(l.get(4))
 
+    l.insert(1, 10)
     print(l)
+    l.insert(8, 11)
+    print(l)
+
+
 
 if __name__ == '__main__':
     main()
